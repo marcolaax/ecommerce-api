eb38b30fde908fdaafb6c2d7911508d1382623ea
Marcos Felipe <notmarcola@gmail.com>
2026-05-27 18:07:23 -0300

    feat: adiciona campos marca, disponibilidade e imagem no produto

--- EcommerceApi/Models/Produto.cs
+++ EcommerceApi/Models/Produto.cs
@@ -11,6 +11,9 @@ public class Produto
     public string Nome { get; set; } = string.Empty;
     public string Descricao { get; set; } = string.Empty;
     public string Categoria { get; set; } = string.Empty;
+    public string Marca { get; set; } = string.Empty;
     public decimal Preco { get; set; }
     public int Estoque { get; set; }
+    public bool Disponivel { get; set;} = true;
+    public string ImageUrl { get; set;} = string.Empty;
 }
